0a574f1e13e779fa01b8900987a5d6edda4af631
EAGnR <edwin_gnr@hotmail.com>
2019-03-22 23:23:52 -0400

    Initial claw functionality -Susanna

--- clawbot-color-fetcher/auto_claw.c
+++ clawbot-color-fetcher/auto_claw.c
@@ -19,85 +19,16 @@ Port 7              distanceSensor      VEX IQ Distance         Distance Sensor 
 Port 1              rightMotor          VEX IQ Motor            Left side motor (reversed)
 Port 6              leftMotor           VEX IQ Motor            Right side motor
 ------------------------------------------------------------------------------------------------*/
-#include "movement.h"
+#include "claw.h"
 
 task main()
 {
-	while(1)
-	{
-		sleep(SENSOR_DELAY);
-
-		float leftDist = 0.0;
-		float rightDist = 0.0;
-		float backDist = 0.0;
-		float forwardDist = getDistanceValue(distanceSensor);
-
-		// If distance sensor is more than 300mm (30cm) away.
-		if(forwardDist > 300.0 && getBumperValue(frontBumper) == 0)
-		{
-			moveForward(MAX_SPEED);
-		}
-		else
-		{
-			if (getBumperValue(frontBumper) == 1)
-			{
-				stopMotionImmediate();
-			}
-			else
-			{
-				stopMotion();
-			}
-			sleep(ACTUATOR_DELAY);
-
-			moveBackward(MAX_SPEED / 2);
-			sleep(1000);
-			stopMotion();
-			sleep(ACTUATOR_DELAY);
-
-			turnLeft(90.0);
-			leftDist = getDistanceValue(distanceSensor);
-			sleep(SENSOR_DELAY);
-
-			turnRight(180.0);
-			rightDist = getDistanceValue(distanceSensor);
-			sleep(SENSOR_DELAY);
-
-			turnRight(90.0);
-			backDist = getDistanceValue(distanceSensor);
-			sleep(SENSOR_DELAY);
-
-			turnLeft(180.0);
-			sleep(SENSOR_DELAY);
-
-			int maxDist = max(max(rightDist, backDist), leftDist);
-
-			if (maxDist > 300.0)
-			{
-				if (leftDist == maxDist)
-				{
-					turnLeft(90.0);
-					sleep(ACTUATOR_DELAY);
-					moveForward(MAX_SPEED);
-				}
-				else if (rightDist == maxDist)
-				{
-					turnRight(90.0);
-					sleep(ACTUATOR_DELAY);
-					moveForward(MAX_SPEED);
-				}
-				else
-				{
-					turnRight(180.0);
-					sleep(ACTUATOR_DELAY);
-					moveForward(MAX_SPEED);
-				}
-				sleep(ACTUATOR_DELAY);
-			}
-			else
-			{
-				stopMotion();
-				sleep(ACTUATOR_DELAY);
-			}
-		}
-	}
+    resetArmPosition();
+    setArmPosition(45);
+    sleep(2000);
+    setArmPosition(180);
+    sleep(2000);
+    setArmPosition(90);
+    sleep(2000);
+    resetArmPosition();
 }
