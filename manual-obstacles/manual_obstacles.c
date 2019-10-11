b8baf03e18602726cea9a7c83fe599aab5c996db
Jorge Euceda <jeuce004@fiu.edu>
2019-10-11 02:31:41 -0400

    simplify method calls

--- manual-obstacles/manual_obstacles.c
+++ manual-obstacles/manual_obstacles.c
@@ -1,12 +1,17 @@
 #pragma config(Sensor, port4,  gyroscope,      sensorVexIQ_Gyro)
 #pragma config(Sensor, port7,  distanceSensor, sensorVexIQ_Distance)
 #pragma config(Sensor, port9,  frontBumper,    sensorVexIQ_Touch)
-#pragma config(Motor,  motor1,          rightMotor,    tmotorVexIQ, openLoop, reversed, encoder)
-#pragma config(Motor,  motor6,          leftMotor,     tmotorVexIQ, openLoop, encoder)
-
+#pragma config(Motor,  motor1,   rightMotor,     tmotorVexIQ, openLoop,   reversed, encoder)
+#pragma config(Motor,  motor6,   leftMotor,      tmotorVexIQ, openLoop,   encoder)
+//NOTE: need to unreverse for usability/building purposes
 #include "manual_obstacles.h"
 
 task main()
 {
-	goForward(1, 40);
+	//move forward one feet
+	goForward(1);
+
+	// now called without degree parameters
+	//	turnLeft();
+	//	turnRight();
 }
